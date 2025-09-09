863f2bf793e604e34da2ff1e4a51afdb35b89fa8
hyunsehyun <99864704+hyunsehyun@users.noreply.github.com>
2025-09-09 15:35:19 +0900

    Add files via upload

--- include/slam_toolbox/slam_toolbox_common.hpp
+++ include/slam_toolbox/slam_toolbox_common.hpp
@@ -29,7 +29,12 @@
 #include <memory>
 #include <fstream>
 
+#include "lifecycle_msgs/msg/state.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "bondcpp/bond.hpp"
+#include "bond/msg/constants.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
 #include "message_filters/subscriber.h"
 #include "tf2_ros/transform_broadcaster.h"
 #include "tf2_ros/transform_listener.h"
@@ -48,6 +53,9 @@
 #include "slam_toolbox/get_pose_helper.hpp"
 #include "slam_toolbox/map_saver.hpp"
 #include "slam_toolbox/loop_closure_assistant.hpp"
+#include "slam_toolbox/msg/pose_graph.hpp"
+#include "slam_toolbox/msg/graph_node.hpp"
+#include "slam_toolbox/msg/graph_edge.hpp"
 
 namespace slam_toolbox
 {
@@ -56,14 +64,24 @@ namespace slam_toolbox
 using namespace ::toolbox_types;  // NOLINT
 using namespace ::karto;  // NOLINT
 
-class SlamToolbox : public rclcpp::Node
+class SlamToolbox : public rclcpp_lifecycle::LifecycleNode
 {
 public:
   explicit SlamToolbox(rclcpp::NodeOptions);
   SlamToolbox();
   virtual ~SlamToolbox();
-  virtual void configure();
   virtual void loadPoseGraphByParams();
+
+  // Create bond connection for nav2 lifecycle manager
+  void createBond();
+  // Destroy bond connection for nav2 lifecycle manager
+  void destroyBond();
+
+  CallbackReturn on_configure(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_cleanup(const rclcpp_lifecycle::State &) override;
+  CallbackReturn on_shutdown(const rclcpp_lifecycle::State & state) override;
 
 protected:
   // threads
@@ -118,6 +136,8 @@ protected:
     const Pose2 & pose,
     const Matrix3 & cov,
     const rclcpp::Time & t);
+  void publishPoseGraph();
+
 
   // pausing bits
   bool isPaused(const PausedApplication & app);
@@ -130,11 +150,15 @@ protected:
   std::unique_ptr<tf2_ros::Buffer> tf_;
   std::unique_ptr<tf2_ros::TransformListener> tfL_;
   std::unique_ptr<tf2_ros::TransformBroadcaster> tfB_;
-  std::unique_ptr<message_filters::Subscriber<sensor_msgs::msg::LaserScan>> scan_filter_sub_;
+  std::unique_ptr<message_filters::Subscriber<sensor_msgs::msg::LaserScan,
+    rclcpp_lifecycle::LifecycleNode>> scan_filter_sub_;
   std::unique_ptr<tf2_ros::MessageFilter<sensor_msgs::msg::LaserScan>> scan_filter_;
-  std::shared_ptr<rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>> sst_;
-  std::shared_ptr<rclcpp::Publisher<nav_msgs::msg::MapMetaData>> sstm_;
-  std::shared_ptr<rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>> pose_pub_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::OccupancyGrid>> sst_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::MapMetaData>> sstm_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<
+      geometry_msgs::msg::PoseWithCovarianceStamped>> pose_pub_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<slam_toolbox::msg::PoseGraph>> pose_graph_pub_;
+
   std::shared_ptr<rclcpp::Service<nav_msgs::srv::GetMap>> ssMap_;
   std::shared_ptr<rclcpp::Service<slam_toolbox::srv::Pause>> ssPauseMeasurements_;
   std::shared_ptr<rclcpp::Service<slam_toolbox::srv::SerializePoseGraph>> ssSerialize_;
@@ -143,6 +167,7 @@ protected:
   // Storage for ROS parameters
   std::string odom_frame_, map_frame_, base_frame_, map_name_, scan_topic_;
   bool use_map_saver_;
+  bool use_lifecycle_manager_;
   rclcpp::Duration transform_timeout_, minimum_time_interval_;
   std_msgs::msg::Header scan_header;
   int throttle_scans_, scan_queue_size_;
@@ -151,7 +176,6 @@ protected:
   double position_covariance_scale_;
   double yaw_covariance_scale_;
   bool first_measurement_, enable_interactive_mode_;
-  bool restamp_tf_;
 
   // Book keeping
   std::unique_ptr<mapper_utils::SMapper> smapper_;
@@ -178,6 +202,9 @@ protected:
   // pluginlib
   pluginlib::ClassLoader<karto::ScanSolver> solver_loader_;
   std::shared_ptr<karto::ScanSolver> solver_;
+
+  // Connection to tell that server is still up
+  std::unique_ptr<bond::Bond> bond_{nullptr};
 };
 
 }  // namespace slam_toolbox
